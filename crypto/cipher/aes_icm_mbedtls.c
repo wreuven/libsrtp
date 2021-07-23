c2f433f5c67fb6e0b3466d1d0094eccf610d22b0
wreuven <wreuven@users.noreply.github.com>
2021-07-23 11:24:55 +0300

    Update aes_icm_mbedtls.c

--- crypto/cipher/aes_icm_mbedtls.c
+++ crypto/cipher/aes_icm_mbedtls.c
@@ -54,7 +54,7 @@
 #include "cipher_test_cases.h"
 
 srtp_debug_module_t srtp_mod_aes_icm = {
-    0,                /* debugging is off by default */
+    1,                /* debugging is off by default */
     "aes icm mbedtls" /* printable module name       */
 };
 
